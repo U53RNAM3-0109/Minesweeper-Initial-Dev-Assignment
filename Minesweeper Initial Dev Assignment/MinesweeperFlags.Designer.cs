5d1bb4585e6305099e7ae9602492024a3d5469db
MorganThomas2 <MorganThomas2@A210583>
2023-10-16 14:19:07 +0100

    Add project files.

---
+++ Minesweeper Initial Dev Assignment/MinesweeperFlags.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace Minesweeper_Initial_Dev_Assignment
+{
+    partial class MinesweeperFlags
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tblpnlMineBoard = new System.Windows.Forms.TableLayoutPanel();
+            this.SuspendLayout();
+            // 
+            // tblpnlMineBoard
+            // 
+            this.tblpnlMineBoard.ColumnCount = 16;
+            this.tblpnlMineBoard.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 36F));
+            this.tblpnlMineBoard.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 36F));
+            this.tblpnlMineBoard.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 36F));
+            this.tblpnlMineBoard.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 36F));
+            this.tblpnlMineBoard.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 36F));
+            this.tblpnlMineBoard.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 36F));
+            this.tblpnlMineBoard.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 36F));
+            this.tblpnlMineBoard.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 36F));
+            this.tblpnlMineBoard.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 36F));
+            this.tblpnlMineBoard.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 36F));
+            this.tblpnlMineBoard.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 36F));
+            this.tblpnlMineBoard.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 36F));
+            this.tblpnlMineBoard.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 36F));
+            this.tblpnlMineBoard.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 36F));
+            this.tblpnlMineBoard.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 36F));
+            this.tblpnlMineBoard.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 36F));
+            this.tblpnlMineBoard.GrowStyle = System.Windows.Forms.TableLayoutPanelGrowStyle.FixedSize;
+            this.tblpnlMineBoard.Location = new System.Drawing.Point(230, 20);
+            this.tblpnlMineBoard.Margin = new System.Windows.Forms.Padding(0);
+            this.tblpnlMineBoard.Name = "tblpnlMineBoard";
+            this.tblpnlMineBoard.RowCount = 16;
+            this.tblpnlMineBoard.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 36F));
+            this.tblpnlMineBoard.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 36F));
+            this.tblpnlMineBoard.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 36F));
+            this.tblpnlMineBoard.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 36F));
+            this.tblpnlMineBoard.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 36F));
+            this.tblpnlMineBoard.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 36F));
+            this.tblpnlMineBoard.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 36F));
+            this.tblpnlMineBoard.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 36F));
+            this.tblpnlMineBoard.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 36F));
+            this.tblpnlMineBoard.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 36F));
+            this.tblpnlMineBoard.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 36F));
+            this.tblpnlMineBoard.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 36F));
+            this.tblpnlMineBoard.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 36F));
+            this.tblpnlMineBoard.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 36F));
+            this.tblpnlMineBoard.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 36F));
+            this.tblpnlMineBoard.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 36F));
+            this.tblpnlMineBoard.Size = new System.Drawing.Size(576, 576);
+            this.tblpnlMineBoard.TabIndex = 1;
+            this.tblpnlMineBoard.TabStop = true;
+            // 
+            // MinesweeperFlags
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(844, 641);
+            this.Controls.Add(this.tblpnlMineBoard);
+            this.Name = "MinesweeperFlags";
+            this.Text = "Minesweeper Flags";
+            this.Load += new System.EventHandler(this.MinesweeperFlags_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tblpnlMineBoard;
+    }
+}
+
---
+++ Minesweeper Initial Dev Assignment/MinesweeperFlags.cs
@@ -0,0 +1,67 @@
+﻿using Minesweeper_Initial_Dev_Assignment.Properties;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.VisualStyles;
+
+namespace Minesweeper_Initial_Dev_Assignment
+{
+    public partial class MinesweeperFlags : Form
+    {
+        public MinesweeperFlags()
+        {
+            InitializeComponent();
+        }
+
+        private void MinesweeperFlags_Load(object sender, EventArgs e)
+        {
+            int rowCount = tblpnlMineBoard.RowCount;
+            int colCount = tblpnlMineBoard.ColumnCount;
+
+            for (int i = 0; i < rowCount; i++)
+            {
+                for (int j = 0; j < colCount; j++) {
+
+                    var cell = new PictureBox();
+
+                    cell.Dock = DockStyle.Top;
+                    cell.Padding = new Padding(0);
+                    cell.Margin = new Padding(0);
+
+                    cell.BackgroundImageLayout = ImageLayout.Stretch;
+                    cell.BackgroundImage = Resources.TILE;
+
+                    cell.MouseClick += new MouseEventHandler(Cell_Click);
+
+                    tblpnlMineBoard.Controls.Add(cell, j, i);
+                }
+            }
+        }
+
+        private void Cell_Click(object sender, MouseEventArgs e)
+        {
+            // handle click event
+            if (e.Button == MouseButtons.Left)
+            {
+                PictureBox cell = sender as PictureBox;
+                cell.BackgroundImage = Resources.TILE_CLEAR;
+            }
+            else if (e.Button == MouseButtons.Right)
+            {
+                PictureBox cell = sender as PictureBox;
+                cell.BackgroundImage = Resources.FLAG_RED;
+            }
+            else if (e.Button == MouseButtons.Middle)
+            {
+                PictureBox cell = sender as PictureBox;
+                cell.BackgroundImage = Resources.FLAG_BLUE;
+            };
+        }
+    }
+}
