d1d7202366ca7c99e5a1a11ecf139f7918791370
PyhcarmU53RNAM3-0109Home <MorganThomas2@exe-coll.ac.uk>
2023-11-06 08:36:56 +0000

    mine clearing logic

--- Minesweeper Initial Dev Assignment/MinesweeperFlags.cs
+++ Minesweeper Initial Dev Assignment/MinesweeperFlags.cs
@@ -20,8 +20,10 @@ namespace Minesweeper_Initial_Dev_Assignment
         public int turnPlayer = 0;
         public List<List<int>> board = new List<List<int>>();
         public bool boardFilled = false;
+        public int mineCount = 32;
         public int boardRows = 0;
         public int boardCols = 0;
+        public List<List<int>> clearedCellLocs = new List<List<int>>();
 
         public MinesweeperFlags()
         {
@@ -122,6 +124,21 @@ namespace Minesweeper_Initial_Dev_Assignment
             }
         }
 
+        private void NextPlayer () {
+            switch (turnPlayer)
+            {
+                case 1:
+                    turnPlayer = 2;
+                    break;
+                case 2:
+                    turnPlayer = 1;
+                    break;
+                default:
+                    MessageBox.Show("nextPlayer error");
+                    break;
+            }
+        }
+
         private List<int> getCellLoc(PictureBox cell) {
             int rowNum = tblpnlMineBoard.GetRow(cell);
             int colNum = tblpnlMineBoard.GetColumn(cell);
@@ -153,6 +170,131 @@ namespace Minesweeper_Initial_Dev_Assignment
 
         private void actionMineClear(PictureBox cell)
         {
+            //initialise board (if not already)
+            //passes first tiled clicked to ensure no instant flagging
+            if (boardFilled != true) { board = generateMineBoard(mineCount, cell); }
+
+            var cellRow = getCellLoc(cell)[0];
+            var cellCol = getCellLoc(cell)[1];
+
+            switch (board[cellRow][cellCol]) {
+                case -1:
+                    //MINE
+                    getPlayer(turnPlayer).flags++;
+                    NextPlayer();
+                    break;
+                case 0:
+                    //EMPTY
+                    ClearSurroundings(cell);
+                    clearedCellLocs.Clear();
+                    break;
+                case 1:
+                case 2:
+                case 3:
+                case 4:
+                case 5:
+                case 6:
+                case 7:
+                case 8:
+                    //NUMBERS
+                    PlaceNumber(cell, board[cellRow][cellCol]);
+                    break;
+                default:
+                    //UNKOWN
+                    break;
+            }
+
+
+        }
+
+        private void ClearSurroundings(PictureBox cell)
+        {
+            var cellRow = getCellLoc(cell)[0];
+            var cellCol = getCellLoc(cell)[1];
+
+            switch (board[cellRow][cellCol])
+            {
+                case -1:
+                    //MINE (gets ignored)
+                    break;
+                case 0:
+                    //EMPTY (recursion)
+                    clearedCellLocs.Add(new List<int> { cellRow, cellCol });
+                    cell.BackgroundImage = Resources.TILE_CLEAR;
+                    for (int i = -1; i <= 1; i++)
+                    {
+                        for (int j = -1; j <= 1; j++) {
+                            if (!clearedCellLocs.Contains(new List<int> { cellRow, cellCol }) && CellInBoardRange(cellRow + i, cellCol + j))
+                            {
+                                //Clear surrounding cells
+                                Point coord = new Point(cellRow, cellCol);
+                                var newCell = tblpnlMineBoard.GetChildAtPoint(coord) as PictureBox;
+
+                                ClearSurroundings(newCell);
+                            }
+                        }
+                    }
+                    break;
+                case 1:
+                case 2:
+                case 3:
+                case 4:
+                case 5:
+                case 6:
+                case 7:
+                case 8:
+                    //NUMBERS (places number)
+                    PlaceNumber(cell, board[cellRow][cellCol]);
+                    break;
+                default:
+                    //UNKOWN (show error box)
+                    MessageBox.Show("Surroundings clearing error");
+                    break;
+            }
+        }
+
+        private void PlaceNumber(PictureBox cell, int number)
+        {
+            switch(number)
+            {
+                case 1:
+                    cell.BackgroundImage = Resources.NUM_1;
+                    break;
+                case 2:
+                    cell.BackgroundImage = Resources.NUM_2;
+                    break;
+                case 3:
+                    cell.BackgroundImage = Resources.NUM_3;
+                    break
+                case 4:
+                    cell.BackgroundImage = Resources.NUM_4;
+                    break;
+                case 5:
+                    cell.BackgroundImage = Resources.NUM_5;
+                    break;
+                case 6:
+                    cell.BackgroundImage = Resources.NUM_6;
+                    break;
+                case 7      
+                    cell.BackgroundImage = Resources.NUM_7;
+                    break;
+                case 8:
+                    cell.BackgroundImage = Resources.NUM_8;
+                    break;
+                default:
+                    MessageBox.Show("Place number error");
+                    break;
+            }
+        }
+
+        private bool CellInBoardRange(int row, int col) {
+            if (row >= 0 && col >= 0 && row <= boardRows && col <= boardCols)
+            {
+                return true;
+            } else
+            {
+                return false;
+            };
         }
     }
     public class Player
